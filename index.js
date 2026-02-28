5cf68da90f10c31b22edbabb2a896621b488ae83
Isaura de Lourdes <isauralourdes23@gmail.com>
2026-02-28 19:28:00 -0300

    criacao dos personagens

---
+++ index.js
@@ -0,0 +1,132 @@
+const personagem = {
+    nome: "Steve Rogers",
+    codinome: "Capitão América",
+    armaPrincipal: "Escudo americano",
+    armaSecundaria: "",
+    velocidade: 85,
+    forca: 75,
+    resistencia: 80,
+    descicrao: function(){
+        return "Nome do personagem: " + this.nome + "\n" +
+        "Codinome do personagem: " + this.codinome + "\n" +
+        "Arma principal: " + this.armaPrincipal + "\n" +
+        "Arma secundária: " + this.armaSecundaria + "\n" +
+        "Nível de velocidade: " + this.velocidade + "\n" +
+        "Nível de força: " + this.forca + "\n" +
+        "Nível de resistência: " + this.resistencia + "\n"
+    }
+}
+
+const personagem2 = {
+    nome: "Peter Parker",
+    codinome: "Homem-Aranha",
+    armaPrincipal: "Lançadores de teia",
+    armaSecundaria: "",
+    velocidade: 90,
+    forca: 90,
+    resistencia: 85,
+    descicrao: function(){
+        return "Nome do personagem: " + this.nome + "\n" +
+        "Codinome do personagem: " + this.codinome + "\n" +
+        "Arma principal: " + this.armaPrincipal + "\n" +
+        "Arma secundária: " + this.armaSecundaria + "\n" +
+        "Nível de velocidade: " + this.velocidade + "\n" +
+        "Nível de força: " + this.forca + "\n" +
+        "Nível de resistência: " + this.resistencia + "\n"
+    }
+}
+
+const personagem3 = {
+    nome: "Bruce Banner",
+    codinome: "Hulk",
+    armaPrincipal: "Punhos",
+    armaSecundaria: "",
+    velocidade: 70,
+    forca: 100,
+    resistencia: 100,
+    descicrao: function(){
+        return "Nome do personagem: " + this.nome + "\n" +
+        "Codinome do personagem: " + this.codinome + "\n" +
+        "Arma principal: " + this.armaPrincipal + "\n" +
+        "Arma secundária: " + this.armaSecundaria + "\n" +
+        "Nível de velocidade: " + this.velocidade + "\n" +
+        "Nível de força: " + this.forca + "\n" +
+        "Nível de resistência: " + this.resistencia + "\n"
+    }
+}
+
+const personagem4 = {
+    nome: "Thor Odinson",
+    codinome: "Thor",
+    armaPrincipal: "Martelo",
+    armaSecundaria: "Raios",
+    velocidade: 100,
+    forca: 100,
+    resistencia: 100,
+    descicrao: function(){
+        return "Nome do personagem: " + this.nome + "\n" +
+        "Codinome do personagem: " + this.codinome + "\n" +
+        "Arma principal: " + this.armaPrincipal + "\n" +
+        "Arma secundária: " + this.armaSecundaria + "\n" +
+        "Nível de velocidade: " + this.velocidade + "\n" +
+        "Nível de força: " + this.forca + "\n" +
+        "Nível de resistência: " + this.resistencia + "\n"
+    }
+}
+
+const personagem5 = {
+    nome: "Natasha Romanoff",
+    codinome: "Viúva Negra",
+    armaPrincipal: "Pistolas",
+    armaSecundaria: "Bastões elétricos",
+    velocidade: 60,
+    forca: 40,
+    resistencia: 55,
+    descicrao: function(){
+        return "Nome do personagem: " + this.nome + "\n" +
+        "Codinome do personagem: " + this.codinome + "\n" +
+        "Arma principal: " + this.armaPrincipal + "\n" +
+        "Arma secundária: " + this.armaSecundaria + "\n" +
+        "Nível de velocidade: " + this.velocidade + "\n" +
+        "Nível de força: " + this.forca + "\n" +
+        "Nível de resistência: " + this.resistencia + "\n"
+    }
+}
+
+const personagem6 = {
+    nome: "Tony Stark",
+    codinome: "Homem de Ferro",
+    armaPrincipal: "Raios da armadura",
+    armaSecundaria: "Mísseis",
+    velocidade: 95,
+    forca: 95,
+    resistencia: 100,
+    descicrao: function(){
+        return "Nome do personagem: " + this.nome + "\n" +
+        "Codinome do personagem: " + this.codinome + "\n" +
+        "Arma principal: " + this.armaPrincipal + "\n" +
+        "Arma secundária: " + this.armaSecundaria + "\n" +
+        "Nível de velocidade: " + this.velocidade + "\n" +
+        "Nível de força: " + this.forca + "\n" +
+        "Nível de resistência: " + this.resistencia + "\n"
+    }
+}
+
+const personagem7 = {
+    nome: "Thanos",
+    codinome: "Thanos",
+    armaPrincipal: "Manopla do infinito",
+    armaSecundaria: "",
+    velocidade: 90,
+    forca: 100,
+    resistencia: 100,
+    descicrao: function(){
+        return "Nome do personagem: " + this.nome + "\n" +
+        "Codinome do personagem: " + this.codinome + "\n" +
+        "Arma principal: " + this.armaPrincipal + "\n" +
+        "Arma secundária: " + this.armaSecundaria + "\n" +
+        "Nível de velocidade: " + this.velocidade + "\n" +
+        "Nível de força: " + this.forca + "\n" +
+        "Nível de resistência: " + this.resistencia + "\n"
+    }
+}
